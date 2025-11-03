eda4e5d13a58fef0499295190426840c35d22014
SNishanth2202 <s.nishanth2202@gmail.com>
2025-11-03 12:53:14 +0530

    frontend

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,9 @@ import express from "express";
 import cors from "cors";
 import { db } from "./db.js";
 import ownerRoutes from "./routes/owners.js";
-// import vehicleRoutes from "./routes/vehicles.js";
-// import registrationRoutes from "./routes/registrations.js";
-// import officeRoutes from "./routes/offices.js";
+import vehicleRoutes from "./routes/vehicles.js";
+import registrationRoutes from "./routes/registrations.js";
+import officeRoutes from "./routes/offices.js";
 
 const app = express();
 app.use(cors());
@@ -12,12 +12,12 @@ app.use(express.json());
 
 // Routes
 app.use("/owners", ownerRoutes);
-// app.use("/vehicles", vehicleRoutes);
-// app.use("/registrations", registrationRoutes);
-// app.use("/offices", officeRoutes);
+app.use("/vehicles", vehicleRoutes);
+app.use("/registrations", registrationRoutes);
+app.use("/offices", officeRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Vehicle Registration API is running 🚗");
+  res.send("Vehicle Registration API is running ");
 });
 
-app.listen(5000, () => console.log("✅ Server running on port 5000"));
+app.listen(5000, () => console.log("Server running on port 5000"));
