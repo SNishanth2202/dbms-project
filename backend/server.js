9b4367573f296d6d3d590a59bdd5ff8065a3df95
SNishanth2202 <s.nishanth2202@gmail.com>
2025-11-03 19:35:59 +0530

    added port

--- backend/server.js
+++ backend/server.js
@@ -27,4 +27,5 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
